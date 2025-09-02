62d56a3c9a7ca6965235caf83cf5867e89e27eb9
Veronika Kulynych <kulynychron@gmail.com>
2025-09-02 09:53:45 +0300

    fix

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,7 +1,6 @@
 
 import styles from "./VoteStatus.module.css"
-import type { Votes } from "../../types/votes"
-
+import type { Votes } from "../../types/votes";
 
 interface VoteStatsProps {
   votes: Votes;
